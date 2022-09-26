843ca457b87015fe0491193603d921f3828e9b13
timflee <timflee@hotmail.com>
2022-09-26 18:11:58 +1000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+music.playMelody("C D E - F E D C ", 120)
 let Rainbow_Wheel = neopixel.create(DigitalPin.P15, 13, NeoPixelMode.RGB)
 Rainbow_Wheel.setBrightness(40)
 Rainbow_Wheel.showColor(neopixel.rgb(100, 100, 100))
@@ -5,5 +6,12 @@ Rainbow_Wheel.setPixelColor(6, neopixel.rgb(255, 165, 0))
 Rainbow_Wheel.show()
 music.playMelody("C D E - F E D C ", 120)
 basic.forever(function () {
-	
+    serial.writeValue("light level", input.lightLevel())
+    if (input.lightLevel() > 30) {
+        basic.showIcon(IconNames.Happy)
+        music.playMelody("C5 B B A A B B C5 ", 500)
+    } else {
+        basic.showIcon(IconNames.Asleep)
+        music.playMelody("A E C - E C5 A A ", 120)
+    }
 })
